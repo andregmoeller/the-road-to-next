3f278e35cdb7ffda569370e7c26c208137277d08
andregmoeller <13366062+andregmoeller@users.noreply.github.com>
2025-06-08 18:03:20 +0200

    feat(theme): Implement light and dark mode toggle

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,20 @@
 import { LucideKanban } from "lucide-react";
 import Link from "next/link";
-import { homepath, ticketsPath } from "@/paths";
+import { homePath, ticketsPath } from "@/paths";
+import { ThemeSwitcher } from "./theme/theme-switcher";
 import { buttonVariants } from "./ui/button";
 
 const Header = () => {
   return (
     <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
-      <div>
-        <Link href={homepath()} className={buttonVariants({ variant: "outline" })}>
+      <div className="flex align-items gap-x-2">
+        <Link href={homePath()} className={buttonVariants({ variant: "outline" })}>
           <LucideKanban />
           <h1 className="ml-2 text-lg font-semibold">TicketBounty</h1>
         </Link>
       </div>
-      <div>
+      <div className="flex align-items gap-x-2">
+        <ThemeSwitcher />
         <Link href={ticketsPath()} className={buttonVariants({ variant: "outline" })}>Tickets</Link>
       </div>
     </nav>
