66c2f73507cce10cf8361c8a89625f481fcfce27
andregmoeller <13366062+andregmoeller@users.noreply.github.com>
2025-10-12 21:35:34 +0200

    chore(lint): Correct import order

--- src/features/ticket/actions/update-ticket-status.ts
+++ src/features/ticket/actions/update-ticket-status.ts
@@ -6,10 +6,10 @@ import {
   fromErrorToActionState,
   toActionState,
 } from "@/components/form/utils/to-action-state";
-import { prisma } from "@/lib/prisma";
-import { ticketsPath } from "@/paths";
 import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
 import { isOwner } from "@/features/auth/utils/is-owner";
+import { prisma } from "@/lib/prisma";
+import { ticketsPath } from "@/paths";
 
 export const updateTicketStatus = async (id: string, status: TicketStatus) => {
   const { user } = await getAuthOrRedirect();
